fba696b7b5bafaed4fa08ddc0e10cf34b5a1852c
Kevin Velez <kevin.alejandro.velez@correounivalle.edu.co>
2025-03-24 11:49:29 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-const sequelize = require('./config/database');
+const sequelize = require(path.join(__dirname, 'config', 'database'));
 
 const authRoutes = require('./routes/auth');
 const projectRoutes = require('./routes/projects');
