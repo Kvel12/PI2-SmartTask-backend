2661bef30aa041c09121353f8571c3b38eeb2438
Kevin Velez <kevin.alejandro.velez@correounivalle.edu.co>
2025-03-24 16:26:04 -0500

    upload changes

--- src/config/database.js
+++ src/config/database.js
@@ -1,43 +1,39 @@
-// Importa Sequelize, el ORM que permite interactuar con la base de datos.
-const { Sequelize } = require('sequelize');  
-
-// Carga las variables de entorno desde un archivo .env.
-require('dotenv').config();  
-
-// Importa el logger para mostrar mensajes en la consola.
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
 const logger = require('../logger');
-// Crea una instancia de Sequelize con la configuración de la base de datos.
-const sequelize = new Sequelize(
-  process.env.DB_NAME,  // Nombre de la base de datos
-  process.env.DB_USER,  // Usuario de la base de datos
-  process.env.DB_PASS,  // Contraseña del usuario
-  {
-    host: process.env.DB_HOST,  // Dirección del servidor de la base de datos
-    dialect: 'postgres',  // Especifica que se usará PostgreSQL como base de datos
-    logging: (msg) => logger.info(msg),  // Muestra los mensajes de Sequelize en la consola
+
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  // Usar la URL de conexión completa proporcionada por Render
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // Importante para conexiones SSL en Render
+      }
+    },
+    logging: (msg) => logger.info(msg),
     define: {
-      timestamps: true,  // Agrega automáticamente los campos createdAt y updatedAt a los modelos
-      underscored: true, // Usa snake_case en lugar de camelCase para los nombres de columnas
+      timestamps: true,
+      underscored: true
     }
-  }
-);
-
-// Función para conectar y sincronizar la base de datos
-/*
-const connectDB = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Connection to the database established successfully.');
-
-    if (process.env.DB_SYNC === 'true') {
-      await sequelize.sync({ alter: true }); // Actualiza la base de datos sin perder datos
-      console.log('Database synchronized with the models.');
+  });
+} else {
+  // Configuración de respaldo para desarrollo local
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: process.env.DB_HOST,
+      dialect: 'postgres',
+      logging: (msg) => logger.info(msg),
+      define: {
+        timestamps: true,
+        underscored: true
+      }
     }
-  } catch (error) {
-    console.error('Error connecting to database:', error);
-    process.exit(1); // Detener la aplicación si no puede conectar a la BD
-  }
-};*/
-
-// Exporta la instancia de Sequelize para ser utilizada en otros archivos.
+  );
+}
 module.exports = sequelize;
